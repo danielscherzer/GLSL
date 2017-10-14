0fd62fe9a9c1f0f3d0609331cace66d0bd4970cb
scherzer <scherzer@hs-weingarten.de>
2017-10-14 15:11:34 +0200

    added ContextGL.csproj

---
+++ ContextGL/SharedContextGL.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using System.ComponentModel.Composition;
+
+namespace ContextGL
+{
+	[Export]
+	[PartCreationPolicy(CreationPolicy.Shared)] //default singleton behavior
+	public class SharedContextGL
+	{
+		public SharedContextGL()
+		{
+			window = new GameWindow(1, 1);
+		}
+
+		public IGraphicsContext Context => window.Context;
+
+		private GameWindow window;
+	}
+}
