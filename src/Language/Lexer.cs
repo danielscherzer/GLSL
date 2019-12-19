8598156fd383bea316502c3e83ff9da59ddb7c97
scherzer <scherzer@hs-weingarten.de>
2019-12-19 08:49:23 +0100

    Lexer support for number with trailing digit e.cx.: 1.

--- src/Language/Lexer.cs
+++ src/Language/Lexer.cs
@@ -7,6 +7,8 @@ namespace DMS.GLSL.Language
 {
 	class Lexer<TokenClassificationType>
 	{
+		static readonly Parser<string> NumberWithTrailingDigit = Parse.Number.Then(result => Parse.Char('.').Select(dot => result + dot));
+		static readonly Parser<string> ParserNumber = Parse.DecimalInvariant.Or(NumberWithTrailingDigit);
 		static readonly Parser<string> ParserComment = new CommentParser().AnyComment;
 		static readonly Parser<string> ParserPreprocessor = Parse.Char('#').Once().Concat(Parse.AnyChar.Until(Parse.String(Environment.NewLine))).Text();
 		static readonly Parser<string> ParserIdentifier = Parse.Identifier(Parse.Letter.Or(Parse.Char('_')), Parse.LetterOrDigit.Or(Parse.Char('_')));
@@ -27,7 +29,7 @@ namespace DMS.GLSL.Language
 				return tokenTypes.Identifier;
 			}
 
-			var number = Parse.DecimalInvariant.Select(value => tokenTypes.Number);
+			var number = ParserNumber.Select(value => tokenTypes.Number);
 			var identifier = ParserIdentifier.Select(value => CheckGlslSpecifics(value));
 			var op = ParserOperator.Select(value => tokenTypes.Operator);
 			var token = comment.Or(preprocessor).Or(number).Or(identifier).Or(op);
