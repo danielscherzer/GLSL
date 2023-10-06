00c9c6c52c6e4ac81f8ae13e9f697800b4793916
Daniel Scherzer <scherzer@rwu.de>
2023-10-06 10:47:46 +0200

    quick commit

--- GLSL_Shared/VsLogger/VsOutput.cs
+++ GLSL_Shared/VsLogger/VsOutput.cs
@@ -10,44 +10,60 @@ namespace DMS.GLSL.VsLogger
 	{
 		internal static void StatusBar(string message)
 		{
-			_ = ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+			try
 			{
-				await StatusBarAsync(message).ConfigureAwait(true);
-			});
+				_ = ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+				{
+					await StatusBarAsync(message).ConfigureAwait(true);
+				});
+			}
+			catch { }
 		}
 
 		internal static async System.Threading.Tasks.Task StatusBarAsync(string message)
 		{
-			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+			try
+			{
+				await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-			var statusBar = (IVsStatusbar)Package.GetGlobalService(typeof(SVsStatusbar));
+				var statusBar = (IVsStatusbar)Package.GetGlobalService(typeof(SVsStatusbar));
 
-			if (statusBar is null) return;
+				if (statusBar is null) return;
 
-			statusBar.IsFrozen(out var frozen);
-			if (0 != frozen) statusBar.FreezeOutput(0);
+				statusBar.IsFrozen(out var frozen);
+				if (0 != frozen) statusBar.FreezeOutput(0);
 
-			ErrorHandler.Succeeded(statusBar.SetText(message));
+				ErrorHandler.Succeeded(statusBar.SetText(message));
+			}
+			catch { }
 		}
 
 		internal static async System.Threading.Tasks.Task WindowPaneAsync(string message)
 		{
-			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-			var outputWindowPane = (IVsOutputWindowPane)Package.GetGlobalService(typeof(SVsGeneralOutputWindowPane));
-
-			if (outputWindowPane != null)
+			try
 			{
-				ErrorHandler.Succeeded(outputWindowPane.OutputStringThreadSafe(message + Environment.NewLine));
+				await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+				var outputWindowPane = (IVsOutputWindowPane)Package.GetGlobalService(typeof(SVsGeneralOutputWindowPane));
+
+				if (outputWindowPane != null)
+				{
+					ErrorHandler.Succeeded(outputWindowPane.OutputStringThreadSafe(message + Environment.NewLine));
+				}
 			}
+			catch { }
 		}
 
 		internal static void WindowPane(string message)
 		{
-			_ = ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+			try
+			{
+				_ = ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
 			  {
 				  await WindowPaneAsync(message).ConfigureAwait(true);
 			  });
+			}
+			catch { }
 		}
 	}
 }
